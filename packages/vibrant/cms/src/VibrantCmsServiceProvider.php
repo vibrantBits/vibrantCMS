962be9935c73a54068e35eea0b82a59104940d62
vagrant <vagrant@homestead>
2017-08-02 04:03:16 +0000

    initial commit

---
+++ packages/vibrant/cms/src/VibrantCmsServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Vibrant\Cms;
+
+use Illuminate\Support\ServiceProvider;
+
+class VibrantCmsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+        //
+        include __DIR__.'/routes.php';
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
---
+++ packages/vibrant/cms/src/routes.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by Edgar Escudero Ramírez.
+ * Date: 01/08/2017
+ * Time: 10:00 PM
+ */
+
+
+Route::get('cms', function(){
+    return('Hello from the calculator package!');
+});
+
